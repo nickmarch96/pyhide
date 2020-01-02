43c6bdae50cea79766c37eef8c2b8d1575768230
Nick <nickmarch96@gmail.com>
2020-01-02 15:53:01 -0600

    Reworked the main portion of the program with argparse Added ability to encrypt folders Added checks for import modules

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# pyhide
-A small tool that creates one time files to send to people and unlocked through a password
+# PyHide
+A lightweight tool that creates one time files to send to people that are strong encrypted with a key. This tool is designed with minimal dependencies and takes advantage of standardized cryptography.
+
+## Features
+- AES CBC Encryption
+- PBKDF2 Key Derivation with randomized iterations
+- Automatically zips folders to send batches of files
+- Filenames are encrypted
+This tool encodes the encrypted binary data to inject into the templated python script. This obviously is not an efficent way to transmit data, however I use base 85 for a small size advantage over the standard base 64.
+
+## Dependencies 
+- Python 2.7 or greater
+- [PyCrypto](https://pypi.org/project/pycrypto/ "PyCrypto")
+
+On Windows, [PyCrypto](https://pypi.org/project/pycrypto/ "PyCrypto") is known to fail on install. [PyCryptodome](https://pypi.org/project/pycryptodome/ "PyCryptodome") is fully compatible with this tool.
+
+## Usage
+Encrypting a file
+```
+python pyhide.py -f path/to/file/file.txt
+```
+Encrypting a folder
+```
+python pyhide -d path/to/folder/
+```
+Command-line password input
+```
+python pyhide -f file.txt -pThisIsNotASecurePasswordInput
+```
+
+## Warning
+This is the first tool I have publicly posted. Please feel free to make change requests. 
+**If you find a security vulnerability in this code or a point of weakness in the encryption please let me know and it will be fixed immediately.**
--- pyhide.py
+++ pyhide.py
@@ -1,35 +1,54 @@
+import sys
 import base64
 import getpass
-import sys
 import os
-from Crypto.Cipher import AES
-from Crypto import Random
-from Crypto.Protocol.KDF import PBKDF2
 import random
+import argparse
+import shutil
 
+try:
+	from Crypto.Cipher import AES
+	from Crypto.Protocol.KDF import PBKDF2
+except ImportError:
+	raise Exception("The Crypto library is required to run this script. NIX: 'pip (or pip3) install pycrypto'. Windows: 'pip install pycryptodome'.")
 
 _TEMPLATE = """#! /usr/bin/python3
-from Crypto.Cipher import AES
-from Crypto.Protocol.KDF import PBKDF2
+try:
+	from Crypto.Cipher import AES
+	from Crypto.Protocol.KDF import PBKDF2
+except ImportError:
+	raise Exception("The Crypto library is required to run this script. NIX: 'pip install pycrypto'. Windows: 'pip install pycryptodome'.")
+
 import base64
 import getpass
 
-password = getpass.getpass("Input password: ")
-
 data = {}
-filename = "{}"
+filename = {}
 
 iv = base64.b64decode({})
 salt = base64.b64decode({})
 itr_len = {}
 
+password = getpass.getpass("Input password: ")
 key = PBKDF2(password, salt, AES.block_size, itr_len, None)
+del password
 aes = AES.new(key, AES.MODE_CBC, iv)
 
 raw = aes.decrypt(base64.b64decode(data))
 raw = raw[:-raw[-1]]
 
-with open(filename, "wb") as o:
+fname = aes.decrypt(base64.b64decode(filename))
+fname = fname[:-fname[-1]]
+
+if not fname:
+	raise Exception("File failed to decrypt.")
+
+try:
+	fname = fname.decode()
+except:
+	raise Exception("File failed to decrypt.")
+
+with open(fname, "wb") as o:
     o.write(raw)
 """
 
@@ -41,10 +60,11 @@ class PyHide():
 	"""
 
 	def __init__(self, password):
-		self.__IV = Random.new().read(AES.block_size)
-		self.__SALT = Random.new().read(AES.block_size)
+		self.__IV = os.urandom(AES.block_size)
+		self.__SALT = os.urandom(AES.block_size)
 		self.__ITR_LEN = random.randint(1000, 10000)
 		self.__KEY = PBKDF2(password, self.__SALT, AES.block_size, self.__ITR_LEN, None)
+		del password
 
 		self.aes = AES.new(self.__KEY, AES.MODE_CBC, self.__IV)
 
@@ -61,10 +81,14 @@ class PyHide():
 
 		with open(file, "rb") as f:
 			data = f.read()
+
 		data = self._pad(data)
 		data = base64.b64encode(self.aes.encrypt(data))
 
-		script = _TEMPLATE.format(data, file.split("/")[-1], base64.b64encode(self.__IV), 
+		file = self._pad(file.encode())
+		file = base64.b64encode(self.aes.encrypt(file))
+
+		script = _TEMPLATE.format(data, file, base64.b64encode(self.__IV), 
 							base64.b64encode(self.__SALT), self.__ITR_LEN)
 
 		return script
@@ -76,23 +100,57 @@ class PyHide():
 
 if __name__ == "__main__":
 
-	if len(sys.argv) != 2:
-		print("usage: ./pyhide.py <filename>")
-		exit()
+	parser = argparse.ArgumentParser(description="Standalone File Packager with encryption.")
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument("-f", help="Filename of target.", dest="file")
+	group.add_argument("-d", help="Directory of target. (Will be compressed)", dest="directory")
+	parser.add_argument("-V", "--version", action="version", version="%(prog)s 2.0")
+	parser.add_argument("-p", help="Password for AES encryption.", dest="pwd")
+	args = parser.parse_args()
 
-	filename = sys.argv[1]
+	file = None
 
-	pwd1 = getpass.getpass("Input password: ")
-	pwd2 = getpass.getpass("Confirm password: ")
+	if args.directory:
+		if not os.path.exists(args.directory):
+			raise Exception("'{}' does not exist!".format(args.directory))
 
-	if pwd1 != pwd2:
-		print("Passwords do not match.\nQuitting...")
-		exit()
+		if not os.path.isdir(args.directory):
+			raise Exception("-d DIRECTORY argument was used but '{}' is not a directory.".format(args.directory))
+
+		file = os.path.basename(os.path.normpath(args.directory))
+		shutil.make_archive(file, "zip", args.directory)
+		file += ".zip"
+
+	if args.file:
+		if not os.path.exists(args.file):
+			raise Exception("'{}' does not exist!".format(args.file))
+
+		if not os.path.isfile(args.file):
+			raise Exception("-f FILE argument was used but '{}' is not a file.".format(args.file))
+
+		file = args.file
+
+	pwd = args.pwd
+	del args.pwd
+
+	if not pwd:
+		pwd = getpass.getpass("Input password: ")
+		pwd2 = getpass.getpass("Confirm password: ")
+
+		if pwd != pwd2:
+			raise Exception("Passwords do not match.")
+
+		del pwd2
+
+	e = PyHide(pwd)
+	del pwd
+
+	script = e.create(file)
+
+	if args.directory:
+		os.remove(file)
 
 
-	e = PyHide(pwd1)
-
-	script = e.create(filename)
 
 	with open("payload.py", "w") as o:
 		o.write(script)
